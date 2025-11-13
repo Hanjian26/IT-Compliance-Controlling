fbeab3e2c6a245fc1a320760165eb582190e5c30
Hanjian <ahan.cf.7@gmail.com>
2025-11-13 14:02:40 +0700

    add gambar pada main menu

--- resources/views/main_menu.blade.php
+++ resources/views/main_menu.blade.php
@@ -3,11 +3,10 @@
 @section('title', 'IT Compliance')
 
 @section('content')
-<div style="margin-left: 20px;">
-  <h1>Menu Utama</h1>
-  <p>Selamat datang, {{ Auth::user()->nama }}</p>
-  {{-- <div class="dashboard" style="text-align: center;">
-    <img src="{{ asset('icons/dashboard.jpg') }}?v=2" alt="Logo Dashboard" width="800" height="450">
-  </div> --}}
+<div style="margin-left: 20px; text-align: center;">
+  {{-- <h1>Menu Utama</h1>
+  <p>Selamat datang, {{ Auth::user()->nama }}</p> --}}
+  <img src="{{ asset('home.jpg') }}?v=2" alt="main-menu" style="width: 70%; display: block; margin: 0 auto;">
 </div>
+
 @endsection
