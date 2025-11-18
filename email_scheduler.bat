11f2a8f104e349b9cd3761a2983fc8fb16de7cec
Hanjian <ahan.cf.7@gmail.com>
2025-11-18 09:16:03 +0700

    fix commands email_scheduler.bat

--- email_scheduler.bat
+++ email_scheduler.bat
@@ -1,4 +1,10 @@
+@REM @echo off
+@REM cd C:\xampp\htdocs\PRODEPT\prodept-app
+@REM php artisan schedule:run >> C:\xampp\htdocs\PRODEPT\prodept-app\storage\logs\scheduler.log 2>&1
+@REM exit
+
+
 @echo off
-cd C:\xampp\htdocs\PRODEPT\prodept-app
-php artisan schedule:run >> C:\xampp\htdocs\PRODEPT\prodept-app\storage\logs\scheduler.log 2>&1
+cd /d C:\xampp\htdocs\PRODEPT\prodept-app
+C:\xampp\php\php.exe artisan schedule:run >> storage\logs\scheduler.log 2>&1
 exit
